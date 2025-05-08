5be3a69c558f166712731755611aac0a76aceb86
RoKols2017 <RoKols2017@gmail.com>
2025-05-08 12:20:06 +0300

    Task 2: подключен Telegram-бот с регистрацией через API

---
+++ tg_bot.py
@@ -0,0 +1,33 @@
+import telebot
+import requests
+import os
+from dotenv import load_dotenv
+
+# Загрузка переменных окружения
+load_dotenv()
+
+API_URL = 'http://127.0.0.1:8000/api/register/'  # или через ngrok в проде
+BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    user_id = message.from_user.id
+    username = message.from_user.username or ""
+
+    # POST-запрос в Django API
+    response = requests.post(API_URL, json={
+        "user_id": user_id,
+        "username": username
+    })
+
+    if response.status_code == 201:
+        bot.send_message(message.chat.id, "✅ Вы успешно зарегистрированы!")
+    elif response.status_code == 200:
+        bot.send_message(message.chat.id, "🔁 Вы уже зарегистрированы.")
+    else:
+        bot.send_message(message.chat.id, "⚠️ Произошла ошибка регистрации.")
+
+bot.polling()
+
